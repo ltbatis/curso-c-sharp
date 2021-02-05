22862206b09a0d232d275dc97ffe4475c27080db
ltbatista <ltbatista30@gmail.com>
2021-02-04 23:00:34 -0300

    primeira estrutura de controle e nova pasta

---
+++ CursoCSharp/EstruturasDeControle/EstruturaIf.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturasDeControle {
+    class EstruturaIf {
+        public static void Executar() {
+            bool bomComportamento = false;
+            string entrada;
+
+            Console.Write("Digite a nota do aluno: ");
+            Double.TryParse(Console.ReadLine(), out double nota);
+
+            Console.Write("Possui bom comportamento (S/N): ");
+            entrada = Console.ReadLine();
+
+            bomComportamento = entrada.ToLower() == "s";
+
+            if(nota >= 9.0 && bomComportamento) {
+                Console.WriteLine("Quadro de honra!");
+            }
+        }
+    }
+}
