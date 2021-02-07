3ca022606913fc8970ce1d93eb6f6dc9e616c200
ltbatista <ltbatista30@gmail.com>
2021-02-07 02:17:38 -0300

    finalizando modulo de estruturas

---
+++ CursoCSharp/EstruturasDeControle/EstruturaFor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturasDeControle {
+    class EstruturaFor {
+        public static void Executar() {
+            for (int i = 0; i < 10; ++i) {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
---
+++ CursoCSharp/EstruturasDeControle/EstruturaForEach.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CursoCSharp.EstruturasDeControle {
+    class EstruturaForEach {
+        public static void Executar() {
+            var palavra = "Lucas";
+
+            foreach (var letra in palavra) {
+                Console.WriteLine(letra);
+            }
+        }
+    }
+}
